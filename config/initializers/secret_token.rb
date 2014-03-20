ec046eb2aba00c31037666629ded6239131c3f43
Matt Withoos <mattwithoos@gmail.com>
2014-03-20 17:54:14 +1100

    The beginnings...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Collabco2::Application.config.secret_key_base = 'fae0baef94845ae59800519634d19b02e6302a8bc0a56bd3fc125814195e39923b5d787ce4daad55fa3ccdac4999bcf02b71a85f6c1e7f8a2b22b1874fd24a62'
